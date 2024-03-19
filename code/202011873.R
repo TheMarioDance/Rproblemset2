62ef3674f208fdb26a1de0b67d4c91a7cbaaf94c
Juan Fernando Contreras <157652760+TheMarioDance@users.noreply.github.com>
2024-03-18 20:07:09 -0500

    Update 202011873.R

--- code/202011873.R
+++ code/202011873.R
@@ -11,24 +11,30 @@ R.version.string
 #Obtener ruta al repositorio actual
 getwd() 
 
-## instalar/llamar pacman
+#instalar/llamar pacman
 require(pacman)
 
 
-## uso la función p_load de pacman para instalar/llamar las librerias que se usaran en el problem set
+#uso la función p_load de pacman para instalar/llamar las librerias que se usaran en el problem set
 p_load(rio, # función import/export: permite leer/escribir archivos desde diferentes formatos. 
        skimr, # funcion skim: describe un conjunto de datos
        janitor, # contiene conjuntos de datos.
        dplyr) # renombar variables
 
+##1. Importar/exportar bases de datos
 
+#1.1 Importe las bases de datos Módulo de sitio o ubicación en un objeto llamdo location y Módulo de identificación en un objeto llamado identification.
+#Se importan las bases de datos del repositorio problem set 2 y se colocan en la carpeta input, se cargan desde dicha ubicacion usando import de rio
 identification = import(file="input/Módulo de identificación.dta")
-
 location = import(file="input/Módulo de sitio o ubicación.dta")
 
+#1.2 Exporte a la carpeta output los objetos cargados en el punto anterior, guárdelos como location.rds y identification.rds.
+#Se exportan a la carpeta output en formato rds
 export(x=identification , file="output/identification.rds")
 export(x=location , file="output/location.rds")
 
+
+##2. Generar variables.
 identification=mutate(identification,bussiness_type=case_when(identification$GRUPOS4=="01" ~ "Agricultura" ,
                                                    identification$GRUPOS4=="02" ~ "Industria Manufacturera" ,
                                                    identification$GRUPOS4=="03" ~ "Comercio" ,
